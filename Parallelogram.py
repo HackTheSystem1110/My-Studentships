12d30d09fe4b4e56668c98f3d62e4a57b6e7fd70
TayBox <58475831+TayBox@users.noreply.github.com>
2020-03-30 15:33:23 +0300

    Update Parallelogram.py

--- Parallelogram.py
+++ Parallelogram.py
@@ -1,16 +1,12 @@
 
-
-
 # Right Slash
-def RightSlash(Val):
-    for i in range(Val):
-        print("/",end="") # End is None
+def RightSlash():
+    print("/",end="") # End is None
 
 
 # Left Slash
-def LeftSlash(Val):
-    for i in range(Val):
-        print("\\",end="") # End is none and \\ for Reverse-Slash "\" is SyntaxError
+def LeftSlash():
+    print("\\",end="") # End is none and \\ for Reverse-Slash "\" is SyntaxError
 # Space
 def Space(Val):
     for i in range(Val):
@@ -21,24 +17,23 @@ def NewLine():
     print() # Simple Jump to New Line
 
 
-def TopTrigonal(Val=0,Right=1,Left=1):
+def TopTrigonal(Val=20):
     StSp = Val//2-1 #StartSpace
     for i in range(Val//2):
         Space(StSp-i)
-        RightSlash(Right)
+        RightSlash()
         Space(i*2)
-        LeftSlash(Left)
+        LeftSlash()
         NewLine()
-def BotTrigonal(Val,Right=1,Left=1):
+def BotTrigonal(Val=20):
     StSp = Val-2 #Start-Space
     for i in range(Val//2):
         Space(i)
-        LeftSlash(Left)
+        LeftSlash()
         Space(StSp - (i*2))
-        RightSlash(Right)
+        RightSlash()
         NewLine()
-def Parallelogram(Val,Right=1,Left=1): # Make The Common-Caller
-    TopTrigonal(Val,Right,Left) # Make Top of Parallelogram
-    BotTrigonal(Val,Right,Left) # Make Bot of Parallelogram
-while(True): #Endless Loop
-    Parallelogram(20) # Make 20 Value Parallelogram...
+def Parallelogram(): # Make The Common-Caller
+    TopTrigonal() # Make Top of Parallelogram
+    BotTrigonal() # Make Bot of Parallelogram
+Parallelogram() # Make 20 Value Parallelogram...
